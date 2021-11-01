ab2af57cd6d15e4bd68079421893324581d71f7f
Matt Martin <matt.martin@sine.co>
2021-11-01 21:02:04 +1030

    menu overlay container

--- src/components/MenuOverlay.jsx
+++ src/components/MenuOverlay.jsx
@@ -1,7 +1,7 @@
 export default function MenuOverlay() {
   return (
     <div className="fixed inset-0 h-screen bg-primary-600 pt-20 z-10">
-      Menu Overlay
+      <div className="text-white container mt-8">Menu Overlay</div>
     </div>
   );
 }
