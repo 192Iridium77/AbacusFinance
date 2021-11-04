66d0102a84b400d6016754ee2d07dff95015f428
Matt Martin <matt.martin@sine.co>
2021-11-04 21:04:34 +1030

    footer, resources setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,107 @@
 export default function Footer() {
+  const SiteMap = () => {
+    return (
+      <div className="flex">
+        <div className="w-6/12">
+          <a href="#services" className="block text-lg mt-2">
+            Services
+          </a>
+          <div className="mt-4">
+            <a href="#loans" className="block mt-1">
+              Loans offered
+            </a>
+            <a href="#lenders" className="block mt-1">
+              Lenders
+            </a>
+            <a href="#benefits" className="block mt-1">
+              Benefits of a Broker
+            </a>
+            <a href="#cost" className="block mt-1">
+              Cost of services
+            </a>
+          </div>
+        </div>
+        <div className="w-6/12">
+          <a href="#contact" className="block text-lg mt-2">
+            Contact
+          </a>
+          <div className="mt-4">
+            <a href="#calculator" className="block mt-1">
+              Calculator
+            </a>
+            <a href="#about" className="block mt-1">
+              About Me
+            </a>
+            <a href="#details" className="block mt-1">
+              Contact Details
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const BusinessDetails = () => {
+    return (
+      <div className="text-center">
+        <div>ABACUS BROKER FINANCE PTY LTD </div>
+        <div className="mt-2">
+          BASc (Mathematics & Econometrics)/Cert IV FMB/ Dip FMB/MA Accounting
+          (Training)
+        </div>
+        <div className="mt-2">ABN: 45 647 177 099 </div>
+      </div>
+    );
+  };
+
+  const Logos = () => {
+    return <div>Logos</div>;
+  };
+
+  const Legal = () => {
+    return (
+      <div className="text-sm text-center text-gray-400">
+        Abacus Broker Finance Pty Ltd ABN 45 647 177 099 has been licensed to
+        use the trademarks owned by Choice Aggregation ABN 40 071 979 498.
+        Australian Credit Licence 391237.
+      </div>
+    );
+  };
+
   return (
     <div className="bg-primary-600 py-12 mt-8">
-      <div className="container text-white">Footer Here</div>
+      <div className="container text-white">
+        <div className="mobile">
+          <SiteMap />
+          <div className="mt-12">
+            <BusinessDetails />
+          </div>
+          <div className="mt-12">
+            <Logos />
+          </div>
+          <div className="mt-12">
+            <Legal />
+          </div>
+        </div>
+        <div className="desktop">
+          <div className="flex">
+            <div className="w-6/12">
+              <SiteMap />
+            </div>
+            <div className="w-6/12">
+              <BusinessDetails />
+            </div>
+          </div>
+          <div className="flex mt-12">
+            <div className="w-6/12">
+              <Logos />
+            </div>
+            <div className="w-6/12">
+              <Legal />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,18 @@ export default function Header() {
   const testimonials = [
     {
       image: "",
-      name: "Jane Smith",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel finibus libero, a vulputate nibh.",
+      name: "Josye Henry",
+      text: `On a recommendation from Elders I was put on to Stu. He was sensitive to my needs and kept me up to date on how we were progressing.
+      I needed someone who knew how to deal as an experienced mortgage broker - He didn't disappoint.
+      We had to dig deep to get results, due to circumstances, but with Stu's contacts and knowledge of the banking and loans system my wish was granted.
+      The Australian dream is achievable. If it's a mortgage you are looking for contact Stu William`,
     },
     {
       image: "",
-      name: "Jane Smith",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel finibus libero, a vulputate nibh.",
+      name: "David Schubert",
+      text: `Stu's done a great job and guided me through the issues that lenders were throwing.
+      He is always making sure I am OK and has helped every step of the way right till the end.
+      Bloody awesome.`,
     },
     {
       image: "",
@@ -27,7 +32,7 @@ export default function Header() {
 
   return (
     <div className="container" showArrows={true}>
-      <div className="lg:hidden">
+      <div className="mobile">
         <Swiper className="mySwiper" pagination>
           {testimonials.map((testimonial) => (
             <SwiperSlide>
@@ -38,7 +43,7 @@ export default function Header() {
           ))}
         </Swiper>
       </div>
-      <div className="hidden lg:block">
+      <div className="desktop">
         <div className="flex">
           {testimonials.map((testimonial) => (
             <div className="w-4/12">
